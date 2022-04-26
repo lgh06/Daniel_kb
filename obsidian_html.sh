e01a9ef9dd8d5abc81007907d26f994c703b6d29
Daniel Liu <hnnk@qq.com>
2022-04-26 11:55:05 +0800

    update for cf pages hints

--- obsidian_html.sh
+++ obsidian_html.sh
@@ -1,2 +1,1 @@
-pip install "obsidianhtml==2.0.2" --force-reinstall
-obsidianhtml -i config.yaml
+python3 -m pip install --upgrade pip && python3 -m pip install https://github.com/lgh06/obsidian-html/archive/refs/heads/master.zip && obsidianhtml -i config_cf.yaml
